12fc4e0c28464085c7a29716ca181c17539646f3
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-07-13 11:07:43 -0500

    se añade la funcion isvalid, con useMemo

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 export default function BudgetForm() {
   const [budget, setBudget] = useState(0)
@@ -7,6 +7,9 @@ export default function BudgetForm() {
     setBudget(e.target.valueAsNumber)
   }
 
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
   return (
     <form className="space-y-5">
       <div className="flex flex-col space-y-5">
@@ -19,7 +22,7 @@ export default function BudgetForm() {
         <input
           type="number"
           id="budget"
-          className="w-full bg-white border-gray-200 p-2"
+          className="w-full bg-white border-gray-200 p-2 "
           placeholder="Define tu presupuesto"
           name="budget"
           value={budget}
@@ -30,7 +33,8 @@ export default function BudgetForm() {
       <input
         type="submit"
         value="Definir Presupuesto"
-        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase disabled:opacity-10"
+        disabled={isValid}
       />
     </form>
   )
