cf320051a0702ca65248e6d13ecaf125cd320482
Luis Alberto Huaman <huamanluis808@gmail.com>
2024-07-14 21:25:47 -0500

    se implementan funcionalidad a los budgetTracker para mostrar los datos

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,17 @@
+import { useMemo } from "react"
+import { useBudget } from "../hooks/useBudget"
 import AmountDisplay from "./AmountDisplay"
 
 export default function BudgetTracker() {
+  const { state } = useBudget()
+
+  const totalExpenses = useMemo(
+    () => state.expenses.reduce((total, expense) => expense.amount + total, 0),
+    [state.expenses]
+  )
+
+  const remainingBudget = state.budget - totalExpenses
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
@@ -11,9 +22,9 @@ export default function BudgetTracker() {
         <button className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg">
           Resetear App
         </button>
-        <AmountDisplay label="Presupuesto" amount={300} />
-        <AmountDisplay label="Disponible" amount={200} />
-        <AmountDisplay label="Gastado" amount={100} />
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
+        <AmountDisplay label="Disponible" amount={remainingBudget} />
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
       </div>
     </div>
   )
